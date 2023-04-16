d594ecd946412d37004359ca076b69f1e10dc6be
Stefano Fornari <stefano.fornari@gmail.com>
2023-04-16 08:32:51 +0200

    fixed req on ver in title

--- src/test/java/ste/w3/easywallet/ui/EasyWalletMainUITest.java
+++ src/test/java/ste/w3/easywallet/ui/EasyWalletMainUITest.java
@@ -52,7 +52,7 @@ public class EasyWalletMainUITest extends BaseEasyWalletMain implements TestingC
     @Test
     public void window_title_icon_and_size() throws Exception {
         Pane root = lookup(".root").queryAs(Pane.class);
-        then(stage.getTitle()).isEqualTo("EasyWallet v0.1");
+        then(stage.getTitle()).isEqualTo("EasyWallet v" + System.getProperty("artifactVersion"));
         then(root.getScene().getWidth()).isGreaterThanOrEqualTo(400);
         then(root.getScene().getHeight()).isGreaterThanOrEqualTo(600);
         then(stage.getIcons()).isNotEmpty();
